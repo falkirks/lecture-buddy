4624fd81a5cc87f1861de495c0bc3d4ab70d4609
Yassine <ymissoum1@gmail.com>
2017-03-18 21:48:50 -0700

    Logo implemented

--- src/Join.js
+++ src/Join.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 export default class Join extends React.Component {
     render() {
-        return <b>JOIN SHIT!</b>;
+        return <div>
+
+        </div>;
+
     }
 }
 
