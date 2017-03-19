ec878153f6d82c20d903d43926f1bdc0cdd1c144
GodotB <godotbian@gmail.com>
2017-03-18 22:16:32 -0700

    handling clicks, commit before pull

--- src/Click.js
+++ src/Click.js
@@ -1,11 +1,12 @@
-/**
- * Created by godotbian on 2017-03-18.
- */
 import React from 'react';
 
 export default class Click extends React.Component {
+
+
+
+
     render() {
-        return <button type="button" className="btn btn-primary">{this.props.name}</button>;
+        return <button type="button" className="btn btn-primary" onClick={this.handleClick}>{this.props.name}</button>;
     }
 }
 
--- src/Join.js
+++ src/Join.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import Click from './Click';
 
 export default class Join extends React.Component {
     constructor(props) {
@@ -64,7 +65,22 @@ export default class Join extends React.Component {
 
     render() {
         if(this.state.name != '' && this.state.key != '' && this.state.buttons.length > 0) {
-            return (<b>WE JOINED AND ITS {this.state.name}</b>);
+            return (
+                <div className="container">
+                    <b>WE JOINED AND ITS {this.state.name}</b>
+
+                    <br/>
+                    <div>
+                        <ul className="list-group">
+                            {this.state.buttons.map((btn) => (
+                                <li> < Click name={btn} /> </li>
+                            ))}
+                        </ul>
+                    </div>
+
+
+                </div>
+            );
         }
         else{
             return (
