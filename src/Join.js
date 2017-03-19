98e0d0a64c47753b4647512d46a7d05eae21dcb2
Yassine <ymissoum1@gmail.com>
2017-03-18 22:02:50 -0700

    Join edited

--- src/Join.js
+++ src/Join.js
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField';
 export default class Join extends React.Component {
     render() {
-        return <b>JOIN SHIT!</b>;
-    }
+        return(
+            <div>
+                <TextField hintText=" Key Code"/>
+                <RaisedButton label='Submit'/>
+            </div>
+        )
+    };
 }
 
