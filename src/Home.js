311dfc7ecd881aecc4396a059e40a5a9944bb78a
Elvis Eshikena <Elvis Eshikena>
2017-03-18 19:05:02 -0700

    Button update

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 export default class Home extends React.Component {
     render() {
-        return <ul>
-            <li><a href="#" onClick={e => this.props.onItemSelect('join')}>Join</a> </li>
-            <li><a href="#" onClick={e => this.props.onItemSelect('create')}>Create</a> </li>
-                </ul>;
+        return (
+        <ul>
+            <li><button><a href="#" onClick={e => this.props.onItemSelect('join')}>Join</a></button></li>
+            <li><button><a href="#" onClick={e => this.props.onItemSelect('create')}>Create</a></button></li>
+        </ul>);
     }
 }
 
