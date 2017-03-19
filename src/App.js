61aaa9b59211687ee98607346259fc58cad75391
Elvis Eshikena <Elvis Eshikena>
2017-03-18 22:12:36 -0700

    header and logo

--- src/App.js
+++ src/App.js
@@ -33,9 +33,11 @@ class App extends React.Component {
         <MuiThemeProvider>
           <div className="container">
             <header>
+                <logo>
                 <svg viewBox="0 0 24 24">
                     <path fill="rgba(0,0,0,0.6)" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M12,10A2,2 0 0,0 10,12A2,2 0 0,0 12,14A2,2 0 0,0 14,12A2,2 0 0,0 12,10Z" />
                 </svg>
+                </logo>
                 Lecture Buddy
             </header>
             {this.state.pageId == 'home' ? <Home onItemSelect={this._onItemSelect} /> : ''}
