9e4c3b9770732ad361d2c2dec8bb5a46d4ffed40
GodotB <godotbian@gmail.com>
2017-03-18 17:31:52 -0700

    added Click component for buttons, onClick action to be added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,18 @@ const socket = io.connect();
 socket.on('message', msg => console.log(msg));
 socket.emit('message', {'msg': "hello world"});
 
-class App extends Component {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttons: [],
+        };
+    }
+
+    componentDidMount() {
+        this.setState({buttons: socket.labels}) //TODO: button names
+    }
+
   render() {
     return (
       <div className="App">
@@ -21,6 +32,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+
+          <div>
+              <ul className="list-group">
+                  {this.state.buttons.map((btn) => (
+                      <li> < Click name={btn} /> </li>
+                  ))}
+              </ul>
+          </div>
       </div>
     );
   }
